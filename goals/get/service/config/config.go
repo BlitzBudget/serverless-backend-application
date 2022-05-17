b5a078e43a604e135dd0b47b0ab92d937c1d0f09
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2022-05-17 20:41:26 +0200

    cange name to goal name

--- goals/get/service/config/config.go
+++ goals/get/service/config/config.go
@@ -4,7 +4,7 @@ import "os"
 
 var TableName = os.Getenv("TABLE_NAME")
 var ScanIndexForward = false
-var ProjectionExpression = "goal_achieved, target_amount, target_date, name, sk, pk, creation_date"
+var ProjectionExpression = "goal_achieved, target_amount, target_date, goal_name, sk, pk, creation_date"
 
 const (
 	SkPrefix = "Goal#"
