06b7abcb20c8cb77cc1682c03bd55a0301bd56c4
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2022-05-17 19:36:38 +0200

    change transaction amount to int

--- debt-rules/add/service/models/query_parameter.go
+++ debt-rules/add/service/models/query_parameter.go
@@ -5,7 +5,7 @@ type QueryParameter struct {
 	Pk                *string `validate:"required" json:"pk"`
 	Sk                string  `json:"sk"`
 	TransactionName   *string `validate:"required" json:"transaction_name"`
-	TransactionAmount *string `validate:"required" json:"transaction_amount"`
+	TransactionAmount *int64  `validate:"required" json:"transaction_amount"`
 	CreationDate      *string `json:"creation_date"`
 	UpdatedDate       *string `json:"updated_date"`
 }
--- investment-rules/get/service/models/response_item.go
+++ investment-rules/get/service/models/response_item.go
@@ -2,10 +2,11 @@ package models
 
 // Create struct to hold info about retrieved item
 type ResponseItem struct {
-	Pk              *string `json:"pk"`
-	Sk              *string `json:"sk"`
-	CreationDate    *string `json:"creation_date"`
-	TransactionName *string `json:"transaction_name"`
+	Pk                *string `json:"pk"`
+	Sk                *string `json:"sk"`
+	CreationDate      *string `json:"creation_date"`
+	TransactionName   *string `json:"transaction_name"`
+	TransactionAmount *int64  `validate:"required" json:"transaction_amount"`
 }
 
 type ResponseItems []*ResponseItem
