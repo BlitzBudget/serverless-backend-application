f2ee4d3fbe89ce5a14e3565db66b51d40cf629d2
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2022-05-18 13:26:31 +0200

    Fix debt errors

--- debt/update/service/models/query_parameter.go
+++ debt/update/service/models/query_parameter.go
@@ -2,10 +2,9 @@ package models
 
 // Create struct to hold info about new item
 type QueryParameter struct {
-	DebtedAmount *string `validate:"required" json:":a"`
+	DebtedAmount *int64  `validate:"required" json:":a"`
 	DebtName     *string `validate:"required" json:":n"`
 	CurrentValue *int64  `validate:"required" json:":c"`
 	UpdatedDate  *string `validate:"required" json:":u"`
-	DebtAmount   *int64  `validate:"required" json:":d"`
 	DebtRepaid   *bool   `validate:"required" json:":r"`
 }
