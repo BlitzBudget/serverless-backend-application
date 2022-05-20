d46d1bc8edc6f1f39e1e0c8ca352c28a62f8f78f
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2022-05-20 18:10:20 +0200

    Refine logs

--- dynamodb-stream/debt-link-trigger/add/service/helper/debt-helper.go
+++ dynamodb-stream/debt-link-trigger/add/service/helper/debt-helper.go
@@ -22,9 +22,9 @@ func IncrementDebtRepayment(debtRule *models.DebtRule, transaction *models.Trans
 		return
 	}
 
-	fmt.Printf("incrementDebtRepayment: Debt Retireved is : %v", debt.DebtName)
+	fmt.Printf("incrementDebtRepayment: Debt Retireved is : %v", *debt.DebtName)
 	if *debt.DebtRepaid {
-		fmt.Printf("incrementDebtRepayment: The Debt %v has been repaid %v. \n", debt.DebtName, debt.DebtRepaid)
+		fmt.Printf("incrementDebtRepayment: The Debt %v has been repaid %v. \n", *debt.DebtName, *debt.DebtRepaid)
 		return
 	}
 
