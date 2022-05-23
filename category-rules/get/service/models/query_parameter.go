9f021808962471c9d92bff794ab724005eafa53d
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2022-05-23 18:03:40 +0200

    fetch with wallet id

--- category-rules/get/service/models/query_parameter.go
+++ category-rules/get/service/models/query_parameter.go
@@ -2,5 +2,5 @@ package models
 
 // Create struct to hold info about new item
 type QueryParameter struct {
-	CategoryId *string `validate:"required" json:"categoryId"`
+	WalletId *string `validate:"required" json:"wallet_id"`
 }
--- debt-rules/get/service/models/query_parameter.go
+++ debt-rules/get/service/models/query_parameter.go
@@ -2,5 +2,5 @@ package models
 
 // Create struct to hold info about new item
 type QueryParameter struct {
-	DebtId *string `validate:"required" json:"debtId"`
+	WalletId *string `validate:"required" json:"wallet_id"`
 }
