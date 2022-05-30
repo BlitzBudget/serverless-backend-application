ca5f95aee2b9606b88bf1e7dec0f1388467da4c8
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2022-05-30 17:33:07 +0200

    delete one item

--- delete/delete-one-item/service/models/request_model.go
+++
@@ -1,7 +0,0 @@
-package models
-
-// Create struct to hold info about new item
-type RequestModel struct {
-	Pk *string `validate:"required" json:"pk"`
-	Sk *string `validate:"required" json:"sk"`
-}
