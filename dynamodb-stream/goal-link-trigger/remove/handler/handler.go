6bbdadfbb770b33f7e078e08a09622b6917fb387
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2022-06-14 10:54:52 +0200

    Converting to JSON

--- dynamodb-stream/goal-link-trigger/remove/handler/handler.go
+++ dynamodb-stream/goal-link-trigger/remove/handler/handler.go
@@ -3,12 +3,18 @@ package handler
 import (
 	"add-goal-link/service"
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 )
 
 func HandleRequest(ctx context.Context, request events.DynamoDBEvent) error {
+	b, err := json.Marshal(request.Records)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(b))
 	fmt.Printf("Processing request data for request %v.\n", request.Records)
 	fmt.Printf("Record Length = %v.\n", len(request.Records))
 	fmt.Println("Records:")
