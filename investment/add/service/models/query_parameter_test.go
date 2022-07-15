647f4c5a3ab82eb2d1946b61f1fb9f4a250ee5b3
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2022-07-15 09:29:58 +0200

    add some unit test

---
+++ investment/add/service/models/query_parameter_test.go
@@ -0,0 +1,60 @@
+package models
+
+import (
+	"encoding/json"
+	"strconv"
+	"testing"
+)
+
+func TestQueryParameter(t *testing.T) {
+	pk := "Wallet#2022-05-12T14:40:29.156Z"
+	sk := "Transaction#2022-05-12T20:25:19Z"
+	creationDate := "2022-05-12T20:25:19Z"
+	updatedDate := "2022-05-12T20:25:19Z"
+	currentValue := 500
+	investedAmount := 200
+	investmentName := "investmentName"
+	body := "{\"pk\":\"" + pk + "\",\"sk\":\"" + sk + "\",\"creation_date\":\"" + creationDate + "\",\"current_value\":" + strconv.Itoa(currentValue) + ",\"invested_amount\":\"" + strconv.Itoa(investedAmount) + "\",\"updated_date\":\"" + updatedDate + "\",\"investment_name\":\"" + investmentName + "}"
+	queryParameter := QueryParameter{}
+	err := json.Unmarshal([]byte(body), &queryParameter)
+
+	if *queryParameter.Pk != pk {
+		t.Errorf("QueryParameter: PK do not match, got = %v, want = %v", *queryParameter.Pk, pk)
+		return
+	}
+
+	if queryParameter.Sk != sk {
+		t.Errorf("QueryParameter: SK do not match, got = %v, want = %v", queryParameter.Sk, sk)
+		return
+	}
+
+	if *queryParameter.CreationDate != creationDate {
+		t.Errorf("QueryParameter: Creation Date do not match, got = %v, want = %v", queryParameter.CreationDate, creationDate)
+		return
+	}
+
+	if *queryParameter.UpdatedDate != updatedDate {
+		t.Errorf("QueryParameter: Updated Date do not match, got = %v, want = %v", queryParameter.UpdatedDate, updatedDate)
+		return
+	}
+
+	if *queryParameter.CurrentValue != int64(currentValue) {
+		t.Errorf("QueryParameter: CurrentValue do not match, got = %v, want = %v", *queryParameter.CurrentValue, currentValue)
+		return
+	}
+
+	if *queryParameter.InvestedAmount != int64(investedAmount) {
+		t.Errorf("QueryParameter: Investment Amount do not match, got = %v, want = %v", *queryParameter.InvestedAmount, investedAmount)
+		return
+	}
+
+	if *queryParameter.InvestmentName != investmentName {
+		t.Errorf("QueryParameter: Investment Name do not match, got = %v, want = %v", queryParameter.InvestmentName, investmentName)
+		return
+	}
+
+	if err != nil {
+		t.Errorf("QueryParameter Struct has an error = %v", err)
+		return
+	}
+}
